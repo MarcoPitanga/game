896d36841c802e117939010494b6f0846173a0f5
Marco Aurélio <marco.hub21@gmail.com>
2022-01-01 19:07:36 -0300

    perfil carregado na pagina inicial

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 async function logar(){
     let logUsuario = document.querySelector('#log-usuario').value
     let logSenha = document.querySelector('#log-senha').value
@@ -16,15 +17,21 @@ async function logar(){
     const dados = await resultado.json()
 
     if(typeof dados.id != 'undefined'){
-        var idAtual = dados.id
-        var usuarioAtual = dados.usuario
-        var classeAtual = dados.classe
-    
-        console.log(`ID: ${idAtual}\nUSUARIO: ${usuarioAtual}\nCLASSE: ${classeAtual}`)
+        sessionStorage.setItem('idAtual', dados.id)
+        sessionStorage.setItem('usuarioAtual', dados.usuario)
+        sessionStorage.setItem('classeAtual', dados.classe)
+
         window.location.href = '../html/home.html'
     
     }else{
         document.querySelector('#feedback').innerHTML = 'Login Invalido'
     }
 
+}
+
+if(window.location.href.indexOf('home.html') != -1){
+    document.querySelector('#perfil-id').innerHTML += sessionStorage.getItem('idAtual')
+    document.querySelector('#perfil-nome').innerHTML += sessionStorage.getItem('usuarioAtual')
+    document.querySelector('#perfil-classe').innerHTML += sessionStorage.getItem('classeAtual')
+    document.querySelector('#perfil-img').src += sessionStorage.getItem('classeAtual')+'.png'
 }
